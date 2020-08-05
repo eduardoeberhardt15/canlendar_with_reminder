f157e55ef93a563817343507e76fe8760dc2f82b
Eduardo Eberhardt <eduardoeberhardt@gmail.com>
2020-08-04 22:18:35 -0300

    create arrays to translate function Date number

---
+++ src/components/Calendar/constants.js
@@ -0,0 +1,22 @@
+export const literalMonth = new Array(12);
+literalMonth[0] = "January";
+literalMonth[1] = "February";
+literalMonth[2] = "March";
+literalMonth[3] = "April";
+literalMonth[4] = "May";
+literalMonth[5] = "June";
+literalMonth[6] = "July";
+literalMonth[7] = "August";
+literalMonth[8] = "September";
+literalMonth[9] = "October";
+literalMonth[10] = "November";
+literalMonth[11] = "December";
+
+export const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
